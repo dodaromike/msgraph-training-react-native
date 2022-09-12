9be38f3bcf37109c27ea7009234b3f1039c9ed1a
dodaromike <v-dodaromike@microsoft.com>
2022-09-12 12:00:40 -0700

    Add project files through tutorial 2

---
+++ GraphTutorial/AuthContext.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+// <AuthContextSnippet>
+import * as React from 'react';
+
+type AuthContextType = {
+  signIn: () => Promise<void>;
+  signOut: () => void;
+};
+
+export const AuthContext = React.createContext<AuthContextType>({
+  signIn: async () => {},
+  signOut: () => {},
+});
+// </AuthContextSnippet>
